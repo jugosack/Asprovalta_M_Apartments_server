1cf3a6a35b84f3f730864d0d2ee8ba1df1dec77b
jugosack <jugoslav_ackoski@yahoo.com>
2023-08-03 18:45:09 +0200

    Add chages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   
-#  protect_from_forgery prepend: true
-protect_from_forgery with: :exception, unless: -> { request.format.json? }
+ protect_from_forgery prepend: true
+
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   
