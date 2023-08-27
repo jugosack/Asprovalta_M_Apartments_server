896376421c20c73f2de49a7facb8a1a7682128fd
jugosack <jugoslav_ackoski@yahoo.com>
2023-08-27 07:55:04 +0200

    Add changes in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,9 +33,9 @@ class UsersController < ApplicationController
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
  
  
