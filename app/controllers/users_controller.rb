726182e188cda75dffba8185bd3151f158d872ad
jugosack <jugoslav_ackoski@yahoo.com>
2023-08-27 07:57:03 +0200

    Add changes in user controller create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,14 +13,14 @@ class UsersController < ApplicationController
   end
 
 
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     render json: @user, status: :created
-  #   else
-  #     render json: @user.errors, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
 
  
   def update
