fced4a6a79d1a8d1eee1a490e275fb0a72a5e71d
jugosack <jugoslav_ackoski@yahoo.com>
2023-12-25 18:23:14 +0100

    Fixing rubocop errors

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -117,7 +117,7 @@ class RoomsController < ApplicationController
   #           available_dates: []
   #         }
   #         available_dates_per_room[room.id][:available_dates] << date.to_s
-          
+
   #       end
   #     end
   #   end
@@ -133,66 +133,64 @@ class RoomsController < ApplicationController
   # end
 
   # rubocop:disable Metrics/CyclomaticComplexity
-# rubocop:disable Metrics/PerceivedComplexity
-def check_availability
-  start_date = Date.strptime(params[:start_date], '%Y-%m-%d')
-  end_date = Date.strptime(params[:end_date], '%Y-%m-%d')
-  capacity = params[:capacity].to_i
+  # rubocop:disable Metrics/PerceivedComplexity
+  def check_availability
+    start_date = Date.strptime(params[:start_date], '%Y-%m-%d')
+    end_date = Date.strptime(params[:end_date], '%Y-%m-%d')
+    capacity = params[:capacity].to_i
 
-  today = Date.today
-  if end_date < today
-    render json: { error: 'Invalid date range. End date must be today or in the future.' },
-           status: :unprocessable_entity
-    return
-  end
+    today = Date.today
+    if end_date < today
+      render json: { error: 'Invalid date range. End date must be today or in the future.' },
+             status: :unprocessable_entity
+      return
+    end
 
-  all_rooms = Room.all
-  available_dates_per_room = {}
+    all_rooms = Room.all
+    available_dates_per_room = {}
 
-  (start_date..end_date).each do |date|
-    next if date < today # Skip dates before today
-    
-    all_rooms.each do |room|
-      blocked_dates = room.blocked_dates.map(&:to_date)
-      reserved_date_ranges = Reservation.where('room_id = ? AND (start_date, end_date) OVERLAPS (?, ?)', room.id,
-                                               date, date)
+    (start_date..end_date).each do |date|
+      next if date < today # Skip dates before today
 
-      if room.capacity >= capacity && !(blocked_dates.include?(date) || reserved_date_ranges.present?)
-        daily_price = RoomDailyPrice.find_by(room_id: room.id, date: date)&.price
+      all_rooms.each do |room|
+        blocked_dates = room.blocked_dates.map(&:to_date)
+        reserved_date_ranges = Reservation.where('room_id = ? AND (start_date, end_date) OVERLAPS (?, ?)', room.id,
+                                                 date, date)
 
-        if daily_price.present? && daily_price.positive?
-          available_dates_per_room[room.id] ||= {
-            name: room.name,
-            available_dates: []
-          }
-          
-          available_dates_per_room[room.id][:available_dates] << {
-            date: date.to_s,
-            price: daily_price
-          }
+        if room.capacity >= capacity && !(blocked_dates.include?(date) || reserved_date_ranges.present?)
+          daily_price = RoomDailyPrice.find_by(room_id: room.id, date: date)&.price
+
+          if daily_price.present? && daily_price.positive?
+            available_dates_per_room[room.id] ||= {
+              name: room.name,
+              available_dates: []
+            }
+
+            available_dates_per_room[room.id][:available_dates] << {
+              date: date.to_s,
+              price: daily_price
+            }
+          end
         end
       end
     end
-  end
 
-  simplified_output = available_dates_per_room.transform_values do |room_data|
-    {
-      name: room_data[:name],
-      available_dates: room_data[:available_dates]
+    simplified_output = available_dates_per_room.transform_values do |room_data|
+      {
+        name: room_data[:name],
+        available_dates: room_data[:available_dates]
+      }
+    end
+
+    render json: {
+      start_date: start_date.to_s,
+      end_date: end_date.to_s,
+      available_dates_per_room: simplified_output
     }
+  rescue ArgumentError
+    render json: { error: 'Invalid date format. Please provide dates in the format YYYY-MM-DD.' },
+           status: :unprocessable_entity
   end
-
-  render json: {
-    start_date: start_date.to_s,
-    end_date: end_date.to_s,
-    available_dates_per_room: simplified_output
-  }
-rescue ArgumentError
-  render json: { error: 'Invalid date format. Please provide dates in the format YYYY-MM-DD.' },
-         status: :unprocessable_entity
-end
-
-
 
   # rubocop:enable Metrics/PerceivedComplexity
   # rubocop:enable Metrics/CyclomaticComplexity
